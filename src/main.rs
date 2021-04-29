4680600376bd4a081e52406a7bfa3bf18963fcb4
glider110 <1026316503@qq.com>
2021-04-29 03:25:36 +0000

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,41 @@
 mod util;
 use crossbeam::channel::{bounded, select, Receiver, Sender};
+use regex::Regex;
 use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::env;
 use std::thread;
 use std::time::Duration;
-use regex::Regex;
-
-
-
 
 fn main() {
     thread::sleep(Duration::from_secs(5)); //延时5s启动
     let ws_url =env::var("WS").unwrap();
-    // let ws_url = "ws://127.0.0.1:30010/"; //"ws://10.243.159.138:30010";
+    // let ws_url = "ws://10.243.184.136:30010/"; //"ws://10.243.159.138:30010";
 
-    let (socket_send_tx,message_out)=util::create_socket_channel(&ws_url);
+    let (socket_send_tx, message_out) = util::create_socket_channel(&ws_url);
     let (update_sig_tx, update_sig_rx) = bounded(1);
-    let (item_search_tx, item_search_rx):(Sender<std::string::String>,Receiver<std::string::String>)  = bounded(10);
-    let (search_result_tx, search_result_rx):(Sender<std::string::String>,Receiver<std::string::String>)  = bounded(10);
-
+    let (item_search_tx, item_search_rx): (
+        Sender<std::string::String>,
+        Receiver<std::string::String>,
+    ) = bounded(10);
+    let (search_result_tx, search_result_rx) = bounded(10);
 
     let update_sig_cron = update_sig_tx.clone();
     let cron_loop = thread::spawn(move || {
         //定时更新循环
-        loop{
-            thread::sleep(Duration::from_secs(60*60*24)); //一天更新一次
+        loop {
+            thread::sleep(Duration::from_secs(60 * 60 * 24)); //一天更新一次
             println!("触发定时更新");
             update_sig_cron.send(1).unwrap();
         }
     });
 
-
     let item_search_db_rx = item_search_rx.clone();
     let database_loop = thread::spawn(move || {
         //数据库查询和更新模块
         let mut db_hash = HashMap::new();
-        loop{
-            select!{
+        loop {
+            select! {
                 recv(update_sig_rx) -> _ =>{
                     println!("收到更新信号，将进行数据库更新");
                     db_hash =util::update_db(40);
@@ -48,15 +46,13 @@ fn main() {
                     println!("收到查询信号，查询目标{}",name);
                     let names = util::get_name(&db_hash, &name);
                     let price = util::get_price(names);
-                    let return_str = util::pretty_str(price);
-                    search_result_tx.send(return_str).unwrap();
+                    // let return_str = util::pretty_str(price);
+                    search_result_tx.send(price).unwrap();
                 }
-                
-            }
 
+            }
         }
     });
-
 
     let message_out_jita = message_out.clone();
     let socket_send_tx_jita = socket_send_tx.clone();
@@ -67,39 +63,47 @@ fn main() {
             let raw_message = message_out_jita.recv().unwrap();
             let v: Value = serde_json::from_str(&raw_message).unwrap();
 
-            if let Some(v_) = jita_re.captures(v["message"].as_str().map_or("",|x|x)) {
-                let item_name = v_.get(1).unwrap().as_str();
-                let all_flag = v_.get(2).map_or(None, |x| Some(x.as_str()));
-                println!("{} {:?}",item_name,all_flag);
-                item_search_tx.send(item_name.to_owned()).unwrap();
-
+            if let Some(v_) = jita_re.captures(v["message"].as_str().map_or("", |x| x)) {
                 let user_id = v["user_id"].as_u64().unwrap();
                 let group_id = v["group_id"].as_u64().unwrap();
+
+                let item_name = v_.get(1).unwrap().as_str();
+                let all_flag = v_.get(2).is_some();
+                println!("{} {}", item_name, all_flag);
+                let mut str_to_send = String::new();
+
+                if all_flag {
+                    item_search_tx.send(item_name.to_owned()).unwrap();
+                    let price_vec = search_result_rx.recv().unwrap();
+                    str_to_send =
+                        util::pretty_str(price_vec).map_or("可能真的搜不到？？".to_owned(), |x| x);
+                } else {
+                    item_search_tx.send(item_name.to_owned()).unwrap();
+                    let price_vec = util::filter_price(search_result_rx.recv().unwrap());
+                    str_to_send = util::pretty_str(price_vec).map_or(
+                        r#"未查询到相关物品在售卖,或只查询到涂装,请检查名称输入是否有误
+若要查看所有结果 在命令后加all即可,例如:jita 三钛合金 all"#
+                            .to_owned(),
+                        |x| x,
+                    );
+                }
 
                 let message_to_send = json!({
                     "action": "send_group_msg",
                     "params": {
                         "group_id": group_id,
-                        "message": search_result_rx.recv().unwrap(),
+                        "message": str_to_send,
                     },
                     "echo": "123"
                 })
                 .to_string();
                 socket_send_tx_jita.send(message_to_send).unwrap();
-            }else{
+            } else {
                 println!("没有匹配到")
             }
-
-           
-                
-
-
         }
     });
 
-
-
-    
     let update_sig_setup = update_sig_tx.clone();
     update_sig_setup.send(1).unwrap(); //第一次更新
 
--- src/util/jita.rs
+++ src/util/jita.rs
@@ -1,10 +1,13 @@
+extern crate strsim;
+
 use crossbeam::channel::{bounded, select, Receiver, Sender};
-use levenshtein::levenshtein;
 use num_format::{Locale, ToFormattedString};
+use regex::Regex;
 use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
+use strsim::jaro;
 
 pub fn update_db(all_page: u64) -> HashMap<String, u64> {
     let (hashmap_tx, hashmap_rx) = bounded(40);
@@ -79,96 +82,148 @@ fn get_type_page(page: u64) -> Vec<u64> {
     return_vec
 }
 
-fn get_score(a: &str, b: &str) -> u64 {
-    let lst = levenshtein(a, b) as f64;
-    let word_len = {
-        if a.chars().count() > b.chars().count() {
-            a.chars().count() as f64
-        } else {
-            b.chars().count() as f64
+fn get_score(dic_name: &str, search_name: &str) -> u64 {
+    let mut pattern = dic_name.to_owned();
+    let mut score = 0;
+
+    fn circle_string(string: &mut String) {
+        let a = string.pop().unwrap();
+        string.insert(0, a);
+    }
+
+    for i in 0..dic_name.chars().count() - 1 {
+        let temp_score = (jaro(&pattern, search_name) * 100.0) as u64;
+        if temp_score >= score {
+            score = temp_score
         }
-    };
+        circle_string(&mut pattern);
+    }
 
-    let a_score = ((word_len - lst) / word_len * 100.0) as u64;
-    a_score
+    // let word_len = a.chars().count() +b.chars().count() ;
+    // let a_score =fuzz::partial_ratio("this is a test", "this is a test!");
+
+    // let a_score = ((word_len - lst) / word_len * 100) as u64;
+    score
 }
-
-pub fn get_name(all_item_dic: &HashMap<String, u64>, name: &str) -> Vec<(String, u64)> {
-    let mut b: Vec<(String, u64)> = all_item_dic
+//名称  匹配分数  id
+pub fn get_name(all_item_dic: &HashMap<String, u64>, name: &str) -> Vec<(String, u64, u64)> {
+    let mut b: Vec<(String, u64, u64)> = all_item_dic
         .into_iter()
-        .map(|item| (item.0.to_string(), get_score(item.0, name)))
+        .map(|item| {
+            (
+                item.0.to_string(),
+                get_score(item.0, name),
+                all_item_dic[item.0],
+            )
+        })
         .collect();
 
     b.sort_by(|a, b| b.1.cmp(&a.1));
+    b
+    // let mut c = vec![];
 
-    let mut c = vec![];
+    // let mut current_score = b.get(0).map_or(0, |a| a.1);
+    // let mut max_num = 0;
 
-    // let mut num = 0;
+    // for target_name in b.iter() {
+    //     let score_gap = current_score - target_name.1;
+    //     println!("{} {}", target_name.0, target_name.1);
+    //     if score_gap > 5 || max_num > 50 {
+    //         break;
+    //     } else {
+    //         max_num += 1;
+    //         c.push((target_name.0.to_owned(), all_item_dic[&target_name.0]));
+    //         current_score = target_name.1;
+    //     }
+    // }
 
-    for target_name in b.iter() {
-        if target_name.1 < 20 {
-            break;
-        } else {
-            // num += 1;
-            c.push((target_name.0.to_owned(), all_item_dic[&target_name.0]));
-        }
-        println!("{} {}", target_name.0, target_name.1);
-    }
-    c
+    // // c.sort_by(|a, b| b.1.cmp(&a.1));
+    // c
 }
-
-pub fn get_price(item_vec: Vec<(String, u64)>) -> Vec<(String, (f64, f64))> {
+                               //名称  score  id            //名称   sell  buy  score
+pub fn get_price(item_vec: Vec<(String, u64, u64)>) -> Vec<(String, (f64, f64, u64))> {
     let client = reqwest::blocking::Client::new();
     let mut return_vec = vec![];
     let mut num = 0;
-    for item in item_vec.into_iter() {
-        if num > 10 {
+    let mut try_num = 0;
+    for (item_name,score,id) in item_vec.into_iter() {
+        println!("{},{},{}",item_name,score,id);
+        if num > 10 || score<60 ||try_num>30{
             break;
         } else {
             let url = format!(
                 "https://www.ceve-market.org/api/market/region/10000002/type/{}.json",
-                item.1
+                id
             );
+            try_num+=1;
             let res = client.get(url).send().unwrap().text().unwrap();
             let v: Value = serde_json::from_str(&res).unwrap();
             let sell = v["sell"]["min"].as_f64().unwrap();
             let buy = v["buy"]["max"].as_f64().unwrap();
+            return_vec.push((item_name, (sell, buy, score)));
             if sell == 0.0 {
                 continue;
             }
-            return_vec.push((item.0, (sell, buy)));
             num += 1;
         }
     }
     return_vec
 }
 
-pub fn pretty_str(price_vec: Vec<(String, (f64, f64))>) -> String {
-    let mut whole_sell = 0;
-    let mut whole_buy = 0;
+pub fn pretty_str(price_vec: Vec<(String, (f64, f64, u64))>) -> Option<String> {
+    let mut whole_sell = 0.0;
+    let mut whole_buy = 0.0;
+    if price_vec.len() == 0 {
+        return None
+    }
 
     let mut return_str = String::from("名称：卖/买\n");
 
-    for item in price_vec.into_iter() {
-        if item.1 .0 == 0.0 {
-            continue;
-        } else {
-            whole_sell += item.1 .0 as u64;
-            whole_buy += item.1 .1 as u64;
-            if item.1 .0 < 1000.0 {
-                return_str.push_str(&format!("{} {}/{}\n", &item.0, item.1 .0, item.1 .0));
-                continue;
-            } else {
-                let sell = (item.1 .0 as u64).to_formatted_string(&Locale::en);
-                let buy = (item.1 .1 as u64).to_formatted_string(&Locale::en);
-                return_str.push_str(&format!("{} {}/{}\n", &item.0, sell, buy));
-            }
+    for (num,(item_name, (sell, buy, score))) in price_vec.into_iter().enumerate() {
+        if num>10{
+            break
         }
+        whole_sell += sell;
+        whole_buy += buy;
+        let mut sellstr = sell.to_string();
+        let mut buystr = buy.to_string();
+        if sell > 1000.0 {
+            sellstr = (sell as u64).to_formatted_string(&Locale::en);
+        } 
+        if buy > 1000.0{
+            buystr = (buy as u64).to_formatted_string(&Locale::en);
+        }
+        return_str.push_str(&format!("{} {}/{}\n", item_name, sellstr, buystr));
+        
     }
     return_str.push_str(&format!(
-        "统计： {}/{}\n",
-        whole_sell.to_formatted_string(&Locale::en),
-        whole_buy.to_formatted_string(&Locale::en)
+        "统计： {}/{}",
+        (whole_sell as u64).to_formatted_string(&Locale::en),
+        (whole_buy as u64).to_formatted_string(&Locale::en)
     ));
-    return_str
+
+    Some(return_str)
+}
+
+pub fn filter_price(price_vec: Vec<(String, (f64, f64, u64))>) -> Vec<(String, (f64, f64, u64))> {
+    let mut return_vec = vec![];
+    let tuzhuang_reg = Regex::new(r"涂装").unwrap();
+    let mut current_score = price_vec.get(0).map_or(0, |a| a.1 .2);
+    // let mut max_num = 0;
+    for (item_name, (sell, buy, score)) in price_vec.into_iter() {
+        println!("{},{},{},{}",item_name,sell, buy, score);
+        let score_gap = current_score - score;
+        if score_gap> 9{
+            break
+        }
+        if (tuzhuang_reg.is_match(&item_name) && score < 90)||(sell==0.0 && score < 70) {
+            continue;
+        }
+        
+        current_score=score;
+        return_vec.push((item_name, (sell, buy, score)));
+        
+    }
+    
+    return_vec
 }
